5f3ba1cf5fc47c9711b7e1267b195c9ea1180651
sastoudt <sstoudt@berkeley.edu>
2017-10-02 10:24:54 -0700

    clean up ui

--- inst/shiny-examples/submit-sheet-private/ui.R
+++ inst/shiny-examples/submit-sheet-private/ui.R
@@ -4,19 +4,22 @@ library(shinythemes)
 shinyUI(
   fluidPage(
     theme = shinytheme("cosmo"),
-    titlePanel("Submit a sheet to a private Google Spreadsheet"),
+    titlePanel("Track Your Writing Goals and Progress for a Week"),
     sidebarLayout(
       sidebarPanel(
-        h6(paste("This app is hard-wired to target a private Google Sheet that will collect writing journals from participants.")),
-        h6(paste("You can't visit the Sheet in the browser, because it's",
-                 "private.")),
-        h6("Fail to browse the Sheet: ",
-           a("HERE", href = ss$browser_url, target="_blank")),
+        h4(paste("STAT 157 Communicating with Data: The Art of Writing for Data Science")),
+        h6(paste("UC Berkeley Statistics Department")),
+        h6(paste("Professor Deborah Nolan and Sara Stoudt")),
+        #h6(paste("You can't visit the Sheet in the browser, because it's",
+        #         "private.")),
+        #h6("Fail to browse the Sheet: ",
+        #   a("HERE", href = ss$browser_url, target="_blank")),
+        
+        h4("Download the Writing Journal template to fill out: ",
+           a("HERE", href = "https://docs.google.com/spreadsheets/d/1tIeBBVAFCRpwQXKnwtbD-J0e6qn1LNjyRsgrGMC18B8/edit?usp=sharing", 
+             target="_blank")),
         h6(a("This app is modelled after Jenny Bryan's googlesheets Shiny example",
              href="https://github.com/jennybc/googlesheets/tree/master/inst/shiny-examples/10_read-write-private-sheet",
-             target="_blank")),
-        h6("Download the Writing Journal template to fill out: ",
-           a("HERE", href = "https://docs.google.com/spreadsheets/d/1tIeBBVAFCRpwQXKnwtbD-J0e6qn1LNjyRsgrGMC18B8/edit?usp=sharing", 
              target="_blank")),
         #sliderInput("row", "Row", min = 1, max = n, value = 1, step = 1,
                   #  ticks = FALSE),
@@ -25,8 +28,8 @@ shinyUI(
         #selectInput("contents", "Cell contents",
                    # choices = c("apple", "grape", "banana")),
         #actionButton("submit", "Submit", class = "btn-primary"),
-        
-        fileInput("file1", "Choose your updated Writing Journal File")
+        h4(paste("Help our students see others' writing processes by filling out a writing journal and uploading it.")),
+        fileInput("file1", "Choose your Writing Journal File")
         
         
         #actionButton("reset", "Reset", class = "btn-primary")
