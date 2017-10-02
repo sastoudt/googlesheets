eb36249f46f6233a330d04eff046deb7e23deb4f
sastoudt <sstoudt@berkeley.edu>
2017-10-02 10:15:48 -0700

    setup my key and sheet ids

---
+++ inst/shiny-examples/submit-sheet-private/setup.R
@@ -0,0 +1,10 @@
+library(googlesheets)
+token <- gs_auth(cache = FALSE)
+gd_token()
+saveRDS(token, file = "googlesheets_token.rds")
+
+ 
+ 
+ sheet_key="1CulqEB5A6e0HjLweVKs7j_aPDFzKuYmEVn5tzbZtbsk"
+ 
+ 
